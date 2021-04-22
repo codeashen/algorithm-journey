3d8683291c94d6a1b17ad3a1730f678d4435cb19
CodeAshen <codeashen@qq.com>
2021-04-22 16:17:26 +0800

    update: leetcode 347

--- Play-with-Data-Structures/src/main/java/queue/leetcode/Solution2.java
+++ Play-with-Data-Structures/src/main/java/queue/leetcode/Solution2.java
@@ -1,6 +1,7 @@
 package queue.leetcode;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 
@@ -10,7 +11,7 @@ import java.util.PriorityQueue;
  */
 public class Solution2 {
 
-    private class Freq implements Comparable<Freq> {
+    private class Freq {
         int e;      // 元素
         int frep;   // 频次
 
@@ -18,16 +19,12 @@ public class Solution2 {
             this.e = e;
             this.frep = frep;
         }
+    }
 
+    private class FrepComparator implements Comparator<Freq> {
         @Override
-        public int compareTo(Freq another) {
-            if (this.frep > another.frep) {
-                return 1;
-            } else if (this.frep < another.frep) {
-                return -1;
-            } else {
-                return 0;
-            }
+        public int compare(Freq o1, Freq o2) {
+            return o1.frep - o2.frep;
         }
     }
 
@@ -38,7 +35,7 @@ public class Solution2 {
             map.put(num, map.containsKey(num) ? map.get(num) + 1 : 1);
         }
 
-        PriorityQueue<Freq> queue = new PriorityQueue<>();
+        PriorityQueue<Freq> queue = new PriorityQueue<>(new FrepComparator());
         for (Integer key : map.keySet()) {
             if (queue.size() < k) {
                 queue.add(new Freq(key, map.get(key)));
