d9e876db339bf6a620d203a839bd9f23e1d0c8f7
CodeAshen <codeashen@qq.com>
2021-05-08 19:28:23 +0800

    add: Insertion Sort

---
+++ Play-with-Algorithms/src/main/java/sort/basic/InsertionSort.java
@@ -0,0 +1,65 @@
+package sort.basic;
+
+/**
+ * 插入排序
+ * <p>
+ * 时间复杂度 O(n^2)
+ * 插入排序对近似有序的数组效率很高
+ */
+public class InsertionSort {
+    
+    private InsertionSort() {}
+
+    /**
+     * 写法一：无须部分向有序部分交换移动
+     */
+    public static void sort1(Comparable[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            // 索引 i 左边是局部有序状态
+            for (int j = i; j > 0; j--) {
+                // 将 arr[i] 逐步插入左边局部有序数组的合适位置
+                if (arr[j].compareTo(arr[j - 1]) < 0) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * 写法二：写法一的该编码方式改进
+     */
+    public static void sort2(Comparable[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            // 索引 i 左边是局部有序状态
+            // 将 arr[i] 逐步插入左边局部有序数组的合适位置
+            for (int j = i; j > 0 && arr[j].compareTo(arr[j - 1]) < 0; j--) {
+                swap(arr, j, j - 1);
+            }
+        }
+    }
+
+    /**
+     * 写法三：有序部分逐步右移，腾出待插入的位置，不涉 swap
+     */
+    public static void sort3(Comparable[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            Comparable e = arr[i];   // 记录无须部分第一个元素 e
+            int index = i;              // 存放 e 应该插入的位置 index
+            
+            while (index > 0 && arr[index - 1].compareTo(e) > 0) {
+                // 将有序部分元素一个个右移，直到找到 e 应该插入的位置
+                arr[index] = arr[index - 1];
+                index--;
+            }
+            arr[index] = e;
+        }
+    }
+
+    private static void swap(Object[] arr, int i, int j) {
+        Object t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+}
--- Play-with-Algorithms/src/test/java/sort/SortTest.java
+++ Play-with-Algorithms/src/test/java/sort/SortTest.java
@@ -3,6 +3,7 @@ package sort;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import sort.basic.InsertionSort;
 import sort.basic.SelectionSort;
 
 import static org.junit.Assert.*;
@@ -11,32 +12,91 @@ public class SortTest {
 
     private Integer[] arr;
     private long sortStartTime;
-    
+
     @Before
     public void initArr() {
-        int n = 20000;
-        int rangeL = 0;
-        int rangeR = 100000;
-        
-        arr = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = (int) (Math.random() * (rangeR - rangeL + 1) + rangeL);
-        }
-
+        arr = generateRandomArray(50000, 0, 200);
+        // arr = generateNearlyOrderedArray(50000, 20);
         sortStartTime = System.currentTimeMillis();
     }
 
     @After
     public void printArr() {
-        System.out.println("Completed, takes " + (System.currentTimeMillis() - sortStartTime) + " ms");
-        // for (Integer integer : arr) {
-        //     System.out.print(integer);
-        //     System.out.print(' ');
-        // }
+        System.out.println("time: " + (System.currentTimeMillis() - sortStartTime) + " ms");
+        System.out.println("success: " + isSorted(arr));
+        // printArray(arr);
+    }
+
+    @Test
+    public void selectionSort() {
+        SelectionSort.sort1(arr);
+        // SelectionSort.sort2(arr);
     }
     
     @Test
-    public void selectionSort() {
-        SelectionSort.sort(arr);
+    public void insertionSort() {
+        // InsertionSort.sort2(arr);
+        InsertionSort.sort3(arr);
     }
+
+
+    //region 辅助方法
+
+    /**
+     * 生成有n个元素的随机数组,每个元素的随机范围为[rangeL, rangeR]
+     */
+    private Integer[] generateRandomArray(int n, int rangeL, int rangeR) {
+        assert rangeL <= rangeR;
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = (int) (Math.random() * (rangeR - rangeL + 1) + rangeL);
+        }
+        return arr;
+    }
+
+    /**
+     * 生成一个近乎有序的数组
+     * 首先生成一个含有[0...n-1]的完全有序数组, 之后随机交换swapTimes对数据
+     * swapTimes定义了数组的无序程度:
+     * swapTimes == 0 时, 数组完全有序；swapTimes 越大, 数组越趋向于无序
+     */
+    private Integer[] generateNearlyOrderedArray(int n, int swapTimes) {
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new Integer(i);
+        }
+
+        for (int i = 0; i < swapTimes; i++) {
+            int a = (int) (Math.random() * n);
+            int b = (int) (Math.random() * n);
+            int t = arr[a];
+            arr[a] = arr[b];
+            arr[b] = t;
+        }
+        return arr;
+    }
+
+    /**
+     * 打印arr数组的所有内容
+     */
+    private void printArray(Object[] arr) {
+        for (Object o : arr) {
+            System.out.print(o + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * 判断arr数组是否有序
+     */
+    private boolean isSorted(Comparable[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i].compareTo(arr[i + 1]) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    //endregion
 }
